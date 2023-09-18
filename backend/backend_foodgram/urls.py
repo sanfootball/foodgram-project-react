04f43267edb234760f8a80475858bfdc8c027ec5
Александр Трубников <petrovskii1980@mail.ru>
2023-09-18 04:27:43 +0300

    no redoc

--- backend/backend_foodgram/urls.py
+++ backend/backend_foodgram/urls.py
@@ -12,12 +12,9 @@ urlpatterns = [
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(
         settings.STATIC_URL, document_root=settings.STATIC_ROOT
-    )
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
