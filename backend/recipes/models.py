1da78e340e337a33277b8d1dfa9cbfa131dcaf50
Александр Трубников <petrovskii1980@mail.ru>
2023-09-23 17:41:17 +0300

    valalidation HEX and upload_to

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
+
 from .utils import upload_to
 
 User = get_user_model()
